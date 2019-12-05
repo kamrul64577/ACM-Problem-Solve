e9d379a1a82a09c6534ea24f037a810c3a821e54
kamrul64577 <53050059+kamrul64577@users.noreply.github.com>
2019-12-05 04:22:34 -0800

    Add files via upload

---
+++ Codeforces-4A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int weight;
+    cin>>weight;
+    if(weight%2==0 and weight>2)
+        cout<<"YES"<<endl;
+    else
+        cout<<"NO"<<endl;
+
+    return 0;
+
+
+}
