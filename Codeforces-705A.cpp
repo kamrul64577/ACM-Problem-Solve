c62fe256ccb51394215ec1c77d13051e27ffc0c8
kamrul64577 <53050059+kamrul64577@users.noreply.github.com>
2020-01-15 09:45:43 -0800

    Add files via upload

---
+++ Codeforces-705A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,i;
+    cin>>n;
+    for(i=1; i<=n; i++)
+    {
+        if(i%2!=0)
+            cout<<" I hate";
+        else
+            cout<<" I love";
+        if(i!=n)
+            cout<<" that";
+        else
+            cout<<" it";
+    }
+}
