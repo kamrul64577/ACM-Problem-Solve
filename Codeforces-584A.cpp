5bc04db7f0013cab3364cfcd2930732307f99fac
kamrul64577 <53050059+kamrul64577@users.noreply.github.com>
2020-01-28 10:35:42 -0800

    Add files via upload

---
+++ Codeforces-584A.cpp
@@ -0,0 +1,89 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,t;
+    cin>>n>>t;
+    if(n==1 && t==10)
+        cout<<-1;
+    else
+    {
+        if(t==2)
+        {
+            for(int i=1; i<=n; i++)
+            {
+
+                cout<<"2";
+            }
+
+        }
+        else if(t==3)
+        {
+            for(int i=1; i<=n; i++)
+            {
+
+                cout<<"3";
+            }
+
+        }
+        else if(t==4)
+        {
+            for(int i=1; i<=n; i++)
+            {
+                cout<<"4";
+            }
+
+        }
+        else if(t==5)
+        {
+            for(int i=1; i<=n; i++)
+            {
+                cout<<"5";
+            }
+
+        }
+        else if(t==6)
+        {
+            for(int i=1; i<=n; i++)
+            {
+                cout<<"6";
+            }
+
+        }
+        else if(t==7)
+        {
+            for(int i=1; i<=n; i++)
+            {
+                cout<<"7";
+            }
+
+        }
+        else if(t==8)
+        {
+            for(int i=1; i<=n; i++)
+            {
+                cout<<"8";
+            }
+
+        }
+        else if(t==9)
+        {
+            for(int i=1; i<=n; i++)
+            {
+                cout<<"9";
+            }
+
+        }
+        else if(t==10)
+        {
+            for(int i=1; i<=n; i++)
+            {
+                if(i==1)
+                    cout<<"1";
+                else
+                    cout<<"0";
+            }
+
+        }
+    }
+}
